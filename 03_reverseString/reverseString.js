496725437dffa52db037d88d8451fadecb852733
Matanda Phiri <matandaphiri64@gmail.com>
2024-09-11 04:17:45 -0500

    Solve exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let individualLetters= str.split("");
+    let reversedWord = "";
 
+    for(let i = individualLetters.length - 1; i >= 0; i--){
+        reversedWord += individualLetters[i];
+    }
+
+    return reversedWord;
 };
 
 // Do not edit below this line
